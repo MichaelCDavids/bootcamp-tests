146c0e42d17ca39fa56db7ccc2602a11c4e7caee
michael <michaelcdavids@gmail.com>
2018-03-14 15:15:51 +0200

    Fixed failing test

--- findItemsOver20.js
+++ findItemsOver20.js
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
-var itemList = [
+
+var itemList1 = [
   {name : 'apples', qty : 9},
   {name : 'pears', qty : 37},
   {name : 'bananas', qty : 27},
   {name : 'kiwi', qty : 13}
 ];
-var itemList1 = [
+var itemList2 = [
   {name : 'apples', qty : 30},
   {name : 'pears', qty : 37},
   {name : 'bananas', qty : 27},
@@ -13,28 +13,6 @@ var itemList1 = [
   {name : 'oranges', qty : 21},
   {name : 'kiwi', qty : 32}
 ];
-
-function findItemsOver20(listOfObjects){
-  console.log('');
-  console.log(listOfObjects);
-	var newQuantityObjectList = [];
-	for(var i=0;i<listOfObjects.length; i++){
-  	var listItem = listOfObjects[i];
-    console.log(listItem);
-    var itemQty = listItem.qty;
-    if(itemQty>20){
-      newQuantityObjectList.push(listOfObjects[i]);
-    }
-    console.log(newQuantityObjectList);
-	}
-  console.log("The amount of items over twenty is = "+newQuantityObjectList.length);
-	return newQuantityObjectList.length;
-}
-
-findItemsOver20(itemList);
-
-findItemsOver20(itemList1);
-=======
 function findItemsOver20(listOfObjects){
   	var newQuantityObjectList = [];
   	for(var i=0;i<listOfObjects.length; i++){
@@ -47,4 +25,3 @@ function findItemsOver20(listOfObjects){
 
   	return newQuantityObjectList.length;
 }
->>>>>>> deaf173c71588be8a793d784745a905fb9ebb5a6
--- test/findItemsOver20.test.js
+++ test/findItemsOver20.test.js
@@ -1,9 +1,9 @@
 describe('The findItemsOver20 function',function(){
 
   it('should take in a list of objects and return how many items have quantities over 20',function(){
-    assert.equal(findItemsOver20(itemList),2);
+    assert.equal(findItemsOver20(itemList1),2);
   });
   it('should take in a list of objects and return how many items have quantities over 20',function(){
-    assert.equal(findItemsOver20(itemList1),6);
+    assert.equal(findItemsOver20(itemList2),6);
   });
 });
