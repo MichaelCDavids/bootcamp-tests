aec55eb342eafdd9e4beb5860c45f34aa538edd9
Michael Davids <michaelcdavids@gmail.com>
2018-03-22 02:11:56 +0200

    fixed failing test

--- test/yearsAgo.test.js
+++ test/yearsAgo.test.js
@@ -1,5 +1,5 @@
 describe('The yearsAgo function', function(){
   it('should check how many years ago it was from the current year',function(){
-    assert.equal(yearsAgo(1992),18);
+    assert.equal(yearsAgo(1992),26);
   });
 });
